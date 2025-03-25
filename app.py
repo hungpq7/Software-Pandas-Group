10660eff43758db8c52c05cc0369d3611f61ee1c
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2025-03-25 23:19:16 +0700

    -

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-page_predition = st.Page('prediction/streamlit.py', title='Stock Return Prediction')
+page_prediction = st.Page('prediction/streamlit.py', title='Stock Return Prediction')
 page_chatbot = st.Page('chatbot/streamlit.py', title='Investment Buddy')
 
-pg = st.navigation([page_predition, page_chatbot])
+pg = st.navigation([page_prediction, page_chatbot])
 pg.run()
