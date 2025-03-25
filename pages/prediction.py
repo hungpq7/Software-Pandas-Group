204d80306e973837314ef322d19bcb594275a670
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2025-03-26 00:27:57 +0700

    -

--- pages/prediction.py
+++ pages/prediction.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-from ..prediction.data import ingest_data, init_data
-from ..prediction.plot import plot_candlestick
-from ..prediction.ticker import TICKERS
+from ..src.prediction.data import ingest_data, init_data
+from ..src.prediction.plot import plot_candlestick
+from ..src.prediction.ticker import TICKERS
 
 df = init_data()
 
