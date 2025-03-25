9533b232eb90fa7f33c12e41f3c99fd0be4e01b9
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2025-03-26 00:30:31 +0700

    update box

--- pages/prediction.py
+++ pages/prediction.py
@@ -9,7 +9,7 @@ df = init_data()
 if st.button('Ingest data'):
     df = ingest_data()
 
-ticker = st.radio('Ticker', options=TICKERS)
+ticker = st.selectbox('Ticker', options=TICKERS)
 
 if st.button('Plot candlestick'):
     fig = plot_candlestick()
