f638570adfedc7493bc585dd7ca68aec9cc8c6cc
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2025-03-25 23:22:17 +0700

    refactor

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-page_prediction = st.Page('prediction/streamlit.py', title='Stock Return Prediction')
-page_chatbot = st.Page('chatbot/streamlit.py', title='Investment Buddy')
+page_prediction = st.Page('pages/prediction.py', title='Stock Return Prediction')
+page_chatbot = st.Page('pages/chatbot.py', title='Investment Buddy')
 
 pg = st.navigation([page_prediction, page_chatbot])
 pg.run()
